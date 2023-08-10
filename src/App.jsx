c0f0eea1d1a6ee87c21713df6a8b07bfaf252cbf
sen <minirose42@gmail.com>
2023-08-11 03:51:19 +0900

    20230811 일기 생성,수정,삭제 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,71 @@
 import './App.css'
 import {Routes, Route, Link} from 'react-router-dom';
+import { useReducer, useRef } from 'react';
 import Home from './pages/Home';
 import New from './pages/New';
 import Diary from './pages/Diary';
 import Edit from './pages/Edit';
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE" : {
+      return [action.data, ...state];
+    }
+    case "UPDATE" : {
+      return state.map((it) => {
+        String(it.id) === String(action.data.id) ? { ...action.data } : it
+      })
+    }
+    case "DELETE" : {
+      return state.filter((it) => {String(it.id) !== String(action.targetId)}); // 삭제할 일기 id와 일치하는거 빼고 새 일기 데이터 배열을 만들어 반환
+    }
+    default : {
+      return state;
+    }
+  }
+}
+
+
+
 function App() {
  
+const [data, dispatch] = useReducer(reducer, []);
+// useReducer를 호출해 일기 데이터를 관리할 State 변수 data를 만든다. 두 번째 인수로 빈 배열을 전달하여 일기 데이터의 초깃값을 설정.
+
+const idRef = useRef(0); //인수로 0을 전달해 초깃값 설정
+
+const onCreate = (date, content, emotionId) => {
+  dispatch({
+    type : "CREATE",
+    data : {
+      id : idRef.current,
+      date : new Date(date).getTime(),
+      content,
+      emotionId,
+    }
+  });
+  idRef.current += 1; // idRef의 현재값을 1늘려 다음 일기 생성시 아이디가 중복되지 않도록 함.
+}
+
+const onUpdate = (targetId, date, content, emotionId) => {
+  dispatch({
+    type : "UPDATE",
+    data : {
+      id : targetId,
+      date : new Date(date).getTime(),
+      content,
+      emotionId,
+    },
+  })
+}
+
+const onDelete = (targetId) => {
+  dispatch({
+    type : "DELETE",
+    targetId,
+  })
+}
+
   return (
     <div className="App">
       <Routes>
@@ -15,13 +74,6 @@ function App() {
         <Route path="/diary/:id" element={<Diary/>} />
         <Route path="/edit" element={<Edit/>} />
       </Routes>
-      <div>
-      <Link to={"/"}>Home</Link>
-      <Link to={"/new"}>New</Link>
-      <Link to={"/diary"}>Diary</Link>
-      <Link to={"/edit"}>Edit</Link>
-      </div>
-      
     </div>
   );
 }
---
+++ src/copypages/Home copy.jsx
@@ -0,0 +1,32 @@
+// import React from 'react'
+// import {useSearchParams} from 'react-router-dom'
+// import Button from '../component/Button';
+// import Header from '../component/Header';
+// import Editor from '../component/Editor';
+
+// //useSearchParams 훅을 호출한다. 이 훅은 useState처럼 배열 형태로 값을 반환한다.
+//     //반환값의 첫 요소는 조회, 수정이 가능한 메소드를 포함한 쿼리스트링 객체,
+//     //두 번째 요소는 이 객체를 업데이트하는 함수이다.
+//     //useSearchParams가 반환한 첫 요소에서 sort값을 불러와 콘솔에 출력. 이때 get메소드 사용함.
+
+// const Home = () => {
+    
+//     // const [searchParams, setSearchParams] = useSearchParams();
+//     // console.log(searchParams.get("sort"));
+    
+//   return (
+//     <div>
+//       <Editor 
+//         initData={{
+//           date : new Date().getTime(),  // new Date().getTime() : 현재날짜를 타임스탬프 값으로 바꿔서 전달(후에 월 단위로 일기를 보여줄 때 날짜 비교 연산을 쉽게 하려고)
+//           emotionId : 1,
+//           content : "이전에 작성했던 일기",
+//         }}
+//         onSubmit={() => {
+//           alert("작성완료!");
+//         }}/>
+//     </div>
+//   );
+// };
+
+// export default Home
